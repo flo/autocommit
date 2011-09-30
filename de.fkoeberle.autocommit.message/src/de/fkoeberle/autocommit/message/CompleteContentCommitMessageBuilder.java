9104313b82bcf5a91273e12a9f48d031f60a0a4a
Florian Köberle <florian@fkoeberle.de>
2011-09-30 19:55:56 +0200

    Added a implementation of ICommitMessageBuilder for debug purposes.

---
+++ de.fkoeberle.autocommit.message/src/de/fkoeberle/autocommit/message/CompleteContentCommitMessageBuilder.java
@@ -0,0 +1,56 @@
+package de.fkoeberle.autocommit.message;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.eclipse.core.resources.IProject;
+
+
+public final class CompleteContentCommitMessageBuilder implements
+		ICommitMessageBuilder {
+	StringBuilder stringBuilder = new StringBuilder();
+
+	@Override
+	public String buildMessage() {
+		return stringBuilder.toString();
+	}
+
+	@Override
+	public void addDeletedFile(IProject project, String path,
+			IFileContent oldContent) throws IOException {
+		stringBuilder.append("Deleted ");
+		stringBuilder.append(path);
+		stringBuilder.append(":\n");
+		String content = read(oldContent);
+		stringBuilder.append(content);
+	}
+
+	@Override
+	public void addChangedFile(IProject project, String path,
+			IFileContent oldContent, IFileContent newContent) throws IOException {
+		stringBuilder.append("Modified ");
+		stringBuilder.append(path);
+		stringBuilder.append("\n");
+		stringBuilder.append("Old content:");
+		stringBuilder.append(read(oldContent));	
+		stringBuilder.append("\nNew content:\n");
+		stringBuilder.append(read(newContent));	
+	}
+
+	private String read(IFileContent oldContent) throws IOException {
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream((int)oldContent.getSize());
+		oldContent.copyTo(buffer);
+		String content = new String(buffer.toByteArray());
+		return content;
+	}
+
+	@Override
+	public void addAddedFile(IProject project, String path,
+			IFileContent newContent) throws IOException {
+		stringBuilder.append("Added ");
+		stringBuilder.append(path);
+		stringBuilder.append(":\n");
+		String content = read(newContent);
+		stringBuilder.append(content);
+	}
+}
