6c02e00475d1c307f73f886cc1fcebcd582ea463
Florian Köberle <florian@fkoeberle.de>
2011-10-06 12:56:05 +0200

    Added AddedClassCommitMessageFactory.

---
+++ de.fkoeberle.autocommit.message.java/src/de/fkoeberle/autocommit/message/java/AddedClassCommitMessageFactory.java
@@ -0,0 +1,52 @@
+package de.fkoeberle.autocommit.message.java;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+
+import de.fkoeberle.autocommit.message.AddedFile;
+import de.fkoeberle.autocommit.message.FileSetDelta;
+import de.fkoeberle.autocommit.message.ICommitMessageFactory;
+
+public class AddedClassCommitMessageFactory implements ICommitMessageFactory {
+	private static final Set<String> DOT_JAVA = Collections.singleton("java");
+
+	public AddedClassCommitMessageFactory() {
+	}
+
+	@Override
+	public String build(FileSetDelta delta) {
+		if (!delta.getFileExtensions().equals(DOT_JAVA)) {
+            return null;
+        }
+		if (delta.getChangedFiles().size() > 0) {
+			return null;
+		}
+		if (delta.getRemovedFiles().size() > 0) {
+			return null;
+		}
+		if (delta.getAddedFiles().size() != 1) {
+			return null;
+		}
+
+		AddedFile addedFile = delta.getAddedFiles().get(0);
+		AddedJavaFile addedJavaFile = addedFile.getAdapter(AddedJavaFile.class);
+		JavaFileContent content = addedJavaFile.getNewJavaContent();
+		CompilationUnit compilationUnit = content
+				.getCompilationUnitForReadOnlyPurposes();
+
+		IJavaElement javaElement = compilationUnit.getJavaElement();
+		if (!(javaElement instanceof ICompilationUnit)) {
+			return null;
+		}
+		ICompilationUnit javaData = ((ICompilationUnit) javaElement);
+		IType primaryType = javaData.findPrimaryType();
+		String primaryTypeName = primaryType.getTypeQualifiedName();
+		return "Added " + primaryTypeName;
+	}
+
+}
