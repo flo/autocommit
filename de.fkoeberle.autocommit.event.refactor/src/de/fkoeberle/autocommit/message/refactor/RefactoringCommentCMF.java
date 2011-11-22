4f789166e4c74a9ce224d585be3cf966eb1686b9
Florian Köberle <florian@fkoeberle.de>
2011-11-22 01:46:32 +0100

    Described the RefactoringCommentCMF extension point and added a argument to it.

--- de.fkoeberle.autocommit.event.refactor/src/de/fkoeberle/autocommit/message/refactor/RefactoringCommentCMF.java
+++ de.fkoeberle.autocommit.event.refactor/src/de/fkoeberle/autocommit/message/refactor/RefactoringCommentCMF.java
@@ -1,13 +1,21 @@
 package de.fkoeberle.autocommit.message.refactor;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.StringReader;
 
 import org.eclipse.ltk.core.refactoring.RefactoringDescriptor;
 
+import de.fkoeberle.autocommit.message.CommitMessage;
+import de.fkoeberle.autocommit.message.CommitMessageTemplate;
 import de.fkoeberle.autocommit.message.ICommitMessageFactory;
 import de.fkoeberle.autocommit.message.InjectedBySession;
 
 public class RefactoringCommentCMF implements ICommitMessageFactory {
+
+	@CommitMessage
+	public final CommitMessageTemplate message = new CommitMessageTemplate(
+			"{1}");
 
 	@InjectedBySession
 	private RefactoringDescriptorContainer refactoringDescriptorContainer;
@@ -23,6 +31,9 @@ public class RefactoringCommentCMF implements ICommitMessageFactory {
 		if (comment.equals("")) {
 			return null;
 		}
-		return comment;
+		StringReader stringReader = new StringReader(comment);
+		BufferedReader bufferedReader = new BufferedReader(stringReader);
+		String firstCommentLine = bufferedReader.readLine();
+		return message.createMessageWithArgs(comment, firstCommentLine);
 	}
 }
