5402b48fd883e5ff4db52a783c889658b1eff917
Florian Köberle <florian@fkoeberle.de>
2011-12-18 23:53:07 +0100

    Let SingleChangedSectionView extends AbstractViewWithCache.

--- de.fkoeberle.autocommit.message.tex/src/de/fkoeberle/autocommit/message/tex/SingleChangedSectionView.java
+++ de.fkoeberle.autocommit.message.tex/src/de/fkoeberle/autocommit/message/tex/SingleChangedSectionView.java
@@ -2,19 +2,26 @@ package de.fkoeberle.autocommit.message.tex;
 
 import java.io.IOException;
 
+import de.fkoeberle.autocommit.message.AbstractViewWithCache;
 import de.fkoeberle.autocommit.message.InjectedBySession;
 
-public class SingleChangedSectionView {
+public class SingleChangedSectionView extends
+		AbstractViewWithCache<OutlineNodeDelta> {
 
 	@InjectedBySession
 	private SingleChangedTexFileView singleChangedTexFileView;
 
-	public OutlineNodeDelta getDelta() throws IOException {
+	@Override
+	protected OutlineNodeDelta determineCachableValue() throws IOException {
 		OutlineNodeDelta delta = singleChangedTexFileView.getDelta();
 		if (delta == null) {
 			return delta;
 		}
 		return delta.findMostSpecificDelta();
+	}
+
+	public OutlineNodeDelta getDelta() throws IOException {
+		return getCachableValue();
 	}
 
 }
