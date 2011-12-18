f05f78e9bfa803b9f93424924dc10ab4fd91280d
Florian Köberle <florian@fkoeberle.de>
2011-12-18 16:01:03 +0100

    Some more tests for SingleAddedSectionView

--- de.fkoeberle.autocommit.message.tex.test/src/de/fkoeberle/autocommit/message/tex/SingleAddedSectionViewTest.java
+++ de.fkoeberle.autocommit.message.tex.test/src/de/fkoeberle/autocommit/message/tex/SingleAddedSectionViewTest.java
@@ -150,4 +150,63 @@ public class SingleAddedSectionViewTest {
 		assertEquals(0, addedSectionInfo.getCharactersAddedAfter());
 		assertEquals(0, addedSectionInfo.getCharactersRemoved());
 	}
+
+	@Test
+	public void testAddedSecondSubSection() throws IOException {
+		SingleAddedSectionView view = create(
+				"\\section{A}\\subsection{a}\\section{B}",
+				"\\section{A}\\subsection{a}\\subsection{X}\\section{B}");
+		AddedSectionInfo addedSectionInfo = view.getAddedSectionInfo();
+		assertNotNull(addedSectionInfo);
+		assertEquals("X", addedSectionInfo.getAddedSection().getCaption());
+		assertEquals(0, addedSectionInfo.getCharactersAddedBefore());
+		assertEquals(0, addedSectionInfo.getCharactersAddedAfter());
+		assertEquals(0, addedSectionInfo.getCharactersRemoved());
+	}
+
+	@Test
+	public void testAddedTextAndSubsection() throws IOException {
+		SingleAddedSectionView view = create("\\section{A}\\section{B}",
+				"\\section{A}hello\\subsection{X}\\section{B}");
+		AddedSectionInfo addedSectionInfo = view.getAddedSectionInfo();
+		assertNotNull(addedSectionInfo);
+		assertEquals("X", addedSectionInfo.getAddedSection().getCaption());
+		assertEquals("hello".length(),
+				addedSectionInfo.getCharactersAddedBefore());
+		assertEquals(0, addedSectionInfo.getCharactersAddedAfter());
+		assertEquals(0, addedSectionInfo.getCharactersRemoved());
+	}
+
+	@Test
+	public void testAddedSubsectionAndModifiedNextSubsection()
+			throws IOException {
+		SingleAddedSectionView view = create(
+				"\\section{A}\\subsection{a}\\section{B}",
+				"\\section{A}\\subsection{X}\\subsection{a}hello x\\section{B}");
+		AddedSectionInfo addedSectionInfo = view.getAddedSectionInfo();
+		assertNotNull(addedSectionInfo);
+		assertEquals("X", addedSectionInfo.getAddedSection().getCaption());
+		assertEquals(0, addedSectionInfo.getCharactersAddedBefore());
+		assertEquals("\\subsection{a}hello x".length(),
+				addedSectionInfo.getCharactersAddedAfter());
+		assertEquals("\\subsection{a}".length(),
+				addedSectionInfo.getCharactersRemoved());
+	}
+
+	@Test
+	public void testAddedSubsectionWithTextAndModifySubsectionsAroundIt()
+			throws IOException {
+		SingleAddedSectionView view = create(
+				"\\section{A}\\subsection{a}text of a;\\subsection{b}text of b;\\section{B}",
+				"\\section{A}\\subsection{a}text of a;x comes;\\subsection{X}I am X!\\subsection{b}text after x;text of b;\\section{B}");
+		AddedSectionInfo addedSectionInfo = view.getAddedSectionInfo();
+		assertNotNull(addedSectionInfo);
+		assertEquals("X", addedSectionInfo.getAddedSection().getCaption());
+		assertEquals("x comes;".length(),
+				addedSectionInfo.getCharactersAddedBefore());
+		assertEquals("\\subsection{b}text after x;".length(),
+				addedSectionInfo.getCharactersAddedAfter());
+		assertEquals("\\subsection{b}".length(),
+				addedSectionInfo.getCharactersRemoved());
+	}
 }
