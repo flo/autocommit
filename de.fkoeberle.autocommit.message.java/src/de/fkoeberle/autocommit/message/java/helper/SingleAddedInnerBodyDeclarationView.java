b7bcdc0e42b133e237b2754b8f477c1ba2c91308
Florian Köberle <florian@fkoeberle.de>
2012-02-29 17:44:27 +0100

    Documented SingleAddedInnerBodyDeclarationView

--- de.fkoeberle.autocommit.message.java/src/de/fkoeberle/autocommit/message/java/helper/SingleAddedInnerBodyDeclarationView.java
+++ de.fkoeberle.autocommit.message.java/src/de/fkoeberle/autocommit/message/java/helper/SingleAddedInnerBodyDeclarationView.java
@@ -14,15 +14,21 @@ import org.eclipse.jdt.core.dom.BodyDeclaration;
 
 import de.fkoeberle.autocommit.message.AbstractViewWithCache;
 import de.fkoeberle.autocommit.message.InjectedBySession;
+import de.fkoeberle.autocommit.message.Session;
 import de.fkoeberle.autocommit.message.java.helper.delta.DeclarationListDelta;
 import de.fkoeberle.autocommit.message.java.helper.delta.TypeDelta;
 
+/**
+ * This class provides a method {@link #getAddedDeclaration()}. It should be
+ * used as an field annotated with {@link InjectedBySession} which in turn gets
+ * initialized by a {@link Session} object.
+ * 
+ */
 public class SingleAddedInnerBodyDeclarationView extends
 		AbstractViewWithCache<BodyDeclaration> {
 
 	@InjectedBySession
 	private SingleChangedTypeView singleChangedTypeView;
-
 
 	@Override
 	protected BodyDeclaration determineCachableValue() throws IOException {
@@ -52,6 +58,11 @@ public class SingleAddedInnerBodyDeclarationView extends
 		return declarationListDelta.getAddedDeclarations().get(0);
 	}
 
+	/**
+	 * 
+	 * @return an {@link BodyDeclaration} instance if the only change was the
+	 *         addition of that declaration. Otherwise this method returns null.
+	 */
 	public BodyDeclaration getAddedDeclaration() throws IOException {
 		return getCachableValue();
 	}
